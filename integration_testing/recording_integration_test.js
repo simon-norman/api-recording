4690203f547a080725636e0480d6565566e83e59
Simon Norman <simon.norman@spacelab.local>
2018-09-19 09:50:29 +0100

    Added other get recordings tests to integration tests to simplify lower level tests

--- integration_testing/recording_integration_test.js
+++ integration_testing/recording_integration_test.js
@@ -3,10 +3,10 @@ const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const { getConfigForEnvironment } = require('../config/config');
-const RecordingControllerFactory = require('../controllers/recording_controller');
+const GetRecordingControllerFactory = require('../controllers/get_recording_controller');
 const Recording = require('../models/recording');
 const RecordingRoutesFactory = require('../routes/recording_routes');
-const ServerFactory = require('../server/server.js');
+const ServerFactory = require('../server/server');
 const mongoose = require('mongoose');
 const request = require('supertest');
 
@@ -14,13 +14,19 @@ chai.use(sinonChai);
 const { expect } = chai;
 
 
-describe('Recording_controller', () => {
+describe('Recording_actions', () => {
   let mockRequest;
 
   describe('Get recordings successfully', () => {
     let recordingsData;
     let server;
     let config;
+
+    const setUpRecordingEndpoints = () => {
+      const getRecordingController = GetRecordingControllerFactory(Recording);
+      const recordingRoutes = RecordingRoutesFactory(getRecordingController);
+      server = ServerFactory(recordingRoutes, config.webServer, sinon.stub());
+    };
 
     const ensureRecordingCollectionEmpty = async () => {
       const recordings = await Recording.find({});
@@ -41,12 +47,6 @@ describe('Recording_controller', () => {
           spaceIds: ['1A', '2C'],
         });
       }
-    };
-
-    const setUpRecordingEndpoints = () => {
-      const recordingController = RecordingControllerFactory(Recording);
-      const recordingRoutes = RecordingRoutesFactory(recordingController);
-      server = ServerFactory(recordingRoutes, config.webServer, sinon.stub());
     };
 
     const setUpMockRequest = () => {
@@ -81,12 +81,12 @@ describe('Recording_controller', () => {
     before(async () => {
       config = getConfigForEnvironment(process.env.NODE_ENV);
       await mongoose.connect(config.recordingDatabase.uri, { useNewUrlParser: true });
+
+      setUpRecordingEndpoints();
     });
 
     beforeEach(async () => {
       resetRecordingsData();
-
-      setUpRecordingEndpoints();
 
       setUpMockRequest();
 
@@ -96,9 +96,11 @@ describe('Recording_controller', () => {
     after(async () => {
       await ensureRecordingCollectionEmpty();
       await mongoose.connection.close();
+      process.exit();
     });
 
-    it('should retrieve all recordings for a specified space id and timeframe', async function () {
+    it('should retrieve recordings for a space id and timeframe, excluding any that are less than the specified start time', async function () {
+      recordingsData[0].timestampRecorded = 1537190999999;
       await loadRecordingsIntoDb();
 
       const response = await request(server).get('/recordings')
@@ -108,6 +110,19 @@ describe('Recording_controller', () => {
 
       expect(response.status).equals(200);
       expect(response.body).deep.equals(expectedRecordings);
+      expect(response.body.length).equals(3);
+    });
+
+    it('should exclude, when retrieving recordings, any that are greater than or equal to the specified end time', async function () {
+      recordingsData[0].timestampRecorded = 1537191000002;
+      recordingsData[1].timestampRecorded = 1537191000003;
+      await loadRecordingsIntoDb();
+
+      const response = await request(server).get('/recordings')
+        .query(mockRequest.query);
+
+      expect(response.status).equals(200);
+      expect(response.body.length).equals(2);
     });
   });
 });
