ae837e0d440fcf925ad54f104551fb91009a4333
Simon Norman <01secn@gmail.com>
2018-10-05 10:22:42 +0100

    Added test to check that tokens for other apps cannot be used to authorise api

--- test/auth_test.js
+++ test/auth_test.js
@@ -39,14 +39,35 @@ describe('Auth0 middleware', function () {
       overrideDefaultExpressErrorHandlerToStopLogStacktraceInTests();
     });
 
-    context('and then a request is made WITHOUT an access token', function () {
-      it('should NOT allow the requestor to access ANY endpoints', async function () {
+    context('and then a request is made WITHOUT a valid access token,', function () {
+      const expectAppToRefuseRequestWith401Status = async (invalidTokenHeader) => {
         const response = request(appWithAuthorization)
           .get('/some-resource')
+          .set('authorization', invalidTokenHeader)
           .send();
 
         const responseError = await getErrorFromRejectedPromise(response);
         expect(responseError.status).equals(401);
+      };
+
+      it('like no token at all', async function () {
+        await expectAppToRefuseRequestWith401Status('');
+      });
+
+      it('like a token for another api', async function () {
+        // have set up another test api on 0Auth purely to generate
+        // tokens that should be invalid for this api
+        const tokenForOtherApi = await axios.post(
+          'https://recordings.eu.auth0.com/oauth/token',
+          {
+            grant_type: 'client_credentials',
+            client_id: 'tO3FtdJESKfvT35pgR0fqqpJJaIIEBc6',
+            client_secret: process.env.TEST_APP_WITHOUT_ACCESS_CLIENT_SECRET,
+            audience: 'https://test-api.com',
+          },
+        );
+
+        await expectAppToRefuseRequestWith401Status(`${tokenForOtherApi.data.token_type} ${tokenForOtherApi.data.access_token}`);
       });
     });
 
